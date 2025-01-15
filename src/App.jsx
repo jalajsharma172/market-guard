efa17ee480a004fd551681dd6b9e95ebe4c2dd0f
Jalaj Sharma <jalajsharma172@gmail.com>
2025-01-15 14:24:23 +0530

    Updated Domain Name

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import Home from './Pages/Home';
 import Dashboard from './Pages/Dashboard';
 import ManuAccount from './Pages/ManuAccount';
 import UserAccount from './Pages/UserAccount';
-// import Wallet from './components/Wallet';
-import Wallet1 from './components/Wallet1';
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/dashboard", element: <Dashboard /> },
     { path: "/create/Manu_account", element: <ManuAccount /> },
-    { path: "/create/User_account", element: <UserAccount /> },
-    { path: "/connect/wallet", element: <Wallet1 /> }
+    { path: "/create/User_account", element: <UserAccount /> }
   ]);
   
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
     return (
         
         <div id="main">
-            <p id="Head">Fair Market</p>
-            <p id="subhead">Empowering communities through on-chain funding. </p>
+            <p id="Head">Supply Guard</p>
+            <p id="subhead">Empowering communities through on-chain transactions safety. </p>
             <div className="container">
                 <div className="box">
                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="green" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shield-check w-12 h-12 text-teal-500"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"></path><path d="m9 12 2 2 4-4"></path></svg>
@@ -28,12 +28,19 @@ const Home = () => {
                     <p>- Provide data-driven insights to inspire innovation in production and distribution.</p>
                 </div>
             </div>
+                   
+
             <div id= "End">
                 <div id="dash">
-                <Link to="/dashboard" className='custom-link'>Get Started -&gt;</Link>
+                    <Link to="/dashboard" className='custom-link'>Get Started -&gt;</Link>
                  </div>
+
+    
+
             </div>
-                
+            <div className='more-info'>
+                 <a href="https://jespublication.com/uploads/2024-V15I40104.pdf"> More Info</a>
+                </div> 
         </div>
     );
 };
